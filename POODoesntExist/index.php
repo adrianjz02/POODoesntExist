b21e8e9bf93cac429bf0bbca477b8e2d88c79b40
Adrian <adr.jimenez.ian@gmail.com>
2023-12-12 15:22:12 +0100

    test

--- POODoesntExist/index.php
+++ POODoesntExist/index.php
@@ -5,9 +5,7 @@ require_once 'root.php'; // Assurez-vous que le nom du fichier est correct.
 $router = new Rooter();
 
 // Ajouter des routes
-$router->add('/contact.html', function () {
-    include 'contact.html';
-});
+$router->add('/contact.html', 'handler');
 
 // Exécuter le routeur
 $router->run();
