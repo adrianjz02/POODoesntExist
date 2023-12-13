ca1cbd2209349b380b016d82086a74042814f02d
shkslxiv <kimseonho133@gmail.com>
2023-12-13 20:06:25 +0900

    The Holy Trinity 	modified:   POODoesntExist/ContactController.php

--- POODoesntExist/ContactController.php
+++ POODoesntExist/ContactController.php
@@ -19,3 +19,52 @@ class ContactController {
 // Créer une instance du contrôleur et appeler la fonction de traitement
 $controller = new ContactController();
 $controller->traiterFormulaire();
+
+
+
+<?php
+
+require_once 'Model.php'; // Assuming a Model class is available
+
+class ContactController {
+    private $model;
+
+    public function __construct() {
+        $this->model = new Model(); // Or a specific ContactModel
+    }
+
+    public function create() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Extract data from POST request
+            $nom = $_POST['nom'] ?? '';
+            $email = $_POST['email'] ?? '';
+            $message = $_POST['message'] ?? '';
+            $ville = $_POST['ville'] ?? '';
+            $age = $_POST['age'] ?? '';
+
+            // Validate and save data using the model
+            // This is a placeholder - implement specific logic for saving data
+            $this->model->saveContact($nom, $email, $message, $ville, $age);
+
+            // Redirect or display a success message
+        }
+    }
+
+    public function read() {
+        // Fetch contact entries using the model
+        // This is a placeholder - implement specific logic for fetching data
+        return $this->model->getAllContacts();
+    }
+
+    public function update() {
+        // Placeholder for the update logic
+    }
+
+    public function delete() {
+        // Placeholder for the delete logic
+    }
+
+    public function showContactForm() {
+        include 'contact.php';
+    }
+}
