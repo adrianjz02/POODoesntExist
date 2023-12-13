30dc1d63508e407a37b775bdb461418bff4be62f
Adrian <adr.jimenez.ian@gmail.com>
2023-12-13 11:04:26 +0100

    controlle

--- POODoesntExist/postController.php
+++ POODoesntExist/postController.php
@@ -1,30 +1,24 @@
 <?php
-class PostController
-{
+class PostController {
     private $db; // Connexion à la base de données
 
-    public function __construct($connexionDB)
-    {
+    public function __construct($connexionDB) {
         $this->db = $connexionDB;
     }
 
-    public function create($postData)
-    {
+    public function create($postData) {
 
     }
 
-    public function read($id = null)
-    {
+    public function read($id = null) {
 
     }
 
-    public function update($id, $postData)
-    {
+    public function update($id, $postData) {
 
     }
 
-    public function delete($id)
-    {
+    public function delete($id) {
 
     }
 }
