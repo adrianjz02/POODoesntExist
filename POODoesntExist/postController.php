3008369ecea5bbd91a95dff4181c002df814d978
Adrian <adr.jimenez.ian@gmail.com>
2023-12-13 10:58:04 +0100

    push

--- POODoesntExist/postController.php
+++ POODoesntExist/postController.php
@@ -1,1 +1,25 @@
-ADRIANNNNNNNN
+<?php
+class PostController {
+    private $db; // Connexion à la base de données
+
+    public function __construct($connexionDB) {
+        $this->db = $connexionDB;
+    }
+
+    public function create($postData) {
+
+    }
+
+    public function read($id = null) {
+
+    }
+
+    public function update($id, $postData) {
+
+    }
+
+    public function delete($id) {
+
+    }
+}
+?>
