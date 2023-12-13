4ab6be9a89247a2bbdeeb9a4d134c29555803f8f
Adrian <adr.jimenez.ian@gmail.com>
2023-12-13 12:37:36 +0100

    commit

--- POODoesntExist/ContactController.php
+++ POODoesntExist/ContactController.php
@@ -1,6 +1,8 @@
 <?php
-class ContactController {
-    public function traiterFormulaire() {
+class ContactController
+{
+    public function traiterFormulaire()
+    {
         $nom = $_POST['nom'] ?? '';
         $email = $_POST['email'] ?? '';
         $message = $_POST['message'] ?? '';
@@ -11,8 +13,30 @@ class ContactController {
     }
 
     // Exemple de fonction pour envoyer un email (à compléter)
-    private function envoyerEmail($nom, $email, $message) {
+    private function envoyerEmail($nom, $email, $message)
+    {
         // Code pour envoyer un email
+    }
+    public function read()
+    {
+        // Fetch contact entries using the model
+        // This is a placeholder - implement specific logic for fetching data
+        return $this->model->getAllContacts();
+    }
+
+    public function update()
+    {
+        // Placeholder for the update logic
+    }
+
+    public function delete()
+    {
+        // Placeholder for the delete logic
+    }
+
+    public function showContactForm()
+    {
+        include 'contact.php';
     }
 }
 
@@ -20,51 +44,4 @@ class ContactController {
 $controller = new ContactController();
 $controller->traiterFormulaire();
 
-
-
-<?php
-
-require_once 'Model.php'; // Assuming a Model class is available
-
-class ContactController {
-    private $model;
-
-    public function __construct() {
-        $this->model = new Model(); // Or a specific ContactModel
-    }
-
-    public function create() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // Extract data from POST request
-            $nom = $_POST['nom'] ?? '';
-            $email = $_POST['email'] ?? '';
-            $message = $_POST['message'] ?? '';
-            $ville = $_POST['ville'] ?? '';
-            $age = $_POST['age'] ?? '';
-
-            // Validate and save data using the model
-            // This is a placeholder - implement specific logic for saving data
-            $this->model->saveContact($nom, $email, $message, $ville, $age);
-
-            // Redirect or display a success message
-        }
-    }
-
-    public function read() {
-        // Fetch contact entries using the model
-        // This is a placeholder - implement specific logic for fetching data
-        return $this->model->getAllContacts();
-    }
-
-    public function update() {
-        // Placeholder for the update logic
-    }
-
-    public function delete() {
-        // Placeholder for the delete logic
-    }
-
-    public function showContactForm() {
-        include 'contact.php';
-    }
-}
+?>
