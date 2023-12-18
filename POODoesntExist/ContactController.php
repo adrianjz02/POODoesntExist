592d4a696844e8a1ad8c46fb1d8fca2d1378533c
Adrian <adr.jimenez.ian@gmail.com>
2023-12-18 10:32:56 +0100

    test

--- POODoesntExist/ContactController.php
+++ POODoesntExist/ContactController.php
@@ -3,36 +3,32 @@ class ContactController
 {
     public function traiterFormulaire()
     {
-        $nom = $_POST['nom'] ?? '';
-        $email = $_POST['email'] ?? '';
-        $message = $_POST['message'] ?? '';
-        $ville = $_POST['ville'] ?? '';
-        $age = $_POST['age'] ?? 0;
+        $nom = $_POST['nom'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $ville = $_POST['ville'];
+        $age = $_POST['age'];
 
         $this->envoyerEmail($nom, $email, $message);
     }
 
-    // Exemple de fonction pour envoyer un email (à compléter)
-
     private function envoyerEmail($nom, $email, $message)
     {
-        // Code pour envoyer un email
+
     }
     public function read()
     {
-        // Fetch contact entries using the model
-        // This is a placeholder - implement specific logic for fetching data
-        return $this->model->getAllContacts();
+
     }
 
     public function update()
     {
-        // Placeholder for the update logic
+
     }
 
     public function delete()
     {
-        // Placeholder for the delete logic
+
     }
 
     public function showContactForm()
