54cd059e1eb463d39543ddfe1e159926e655d64a
Adrian <adr.jimenez.ian@gmail.com>
2023-12-19 12:03:36 +0100

    style

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -15,14 +15,14 @@ final class PostController extends BaseController
     public function create()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // Extract and sanitize data from POST request
+
             $nom = htmlspecialchars($_POST['nom'] ?? '');
             $email = htmlspecialchars($_POST['email'] ?? '');
             $message = htmlspecialchars($_POST['message'] ?? '');
             $ville = htmlspecialchars($_POST['ville'] ?? '');
             $age = htmlspecialchars($_POST['age'] ?? '');
 
-            // Render a new Twig page with the submitted data
+
             echo $this->render('post/detail.html.twig', [
                 'nom' => $nom,
                 'email' => $email,
